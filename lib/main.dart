66c3927a04e3486fb3e5633305cbf18e250edc56
hammadx02 <hammadaali02@gmail.com>
2023-09-13 00:46:54 +0500

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,26 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:instagram_clone/responsive/mobile_screen_layout.dart';
 import 'package:instagram_clone/responsive/responsive_layout_screen.dart';
 import 'package:instagram_clone/responsive/web_screen_layout.dart';
 import 'package:instagram_clone/utils/colors.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+        options: const FirebaseOptions(
+      apiKey: 'AIzaSyCZn46zuoVFpcmmLXmC05eHDrzgDgGd4xg',
+      appId: '1:462148851457:web:12ce863902029c1c24ad00',
+      messagingSenderId: '462148851457',
+      projectId: 'instagram-clone-6ecba',
+      storageBucket: 'instagram-clone-6ecba.appspot.com',
+    ));
+  } else {
+    await Firebase.initializeApp();
+  }
+
   runApp(const MyApp());
 }
 
